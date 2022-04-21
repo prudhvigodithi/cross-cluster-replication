eb09a198c855d1efe6f056759ae73481b247bb82
pgodithi <pgodithi@amazon.com>
2022-04-21 11:33:08 -0700

    changed to query system property
    Signed-off-by: pgodithi <pgodithi@amazon.com>

--- buildSrc/src/main/java/org/opensearch/gradle/zipplugin/ZipPublishUtil.java
+++
@@ -1,9 +0,0 @@
-package org.opensearch.gradle.zipplugin;
-
-class ZipPublishUtil {
-
-    static String capitalize(String str) {
-        return str.substring(0, 1).toUpperCase() + str.substring(1);
-    }
-
-}
